376b276e248b0f2d5c0f3d4361b2a9f846736e38
fikrichayma1-glitch <fikrichayma1@gmail.com>
2026-04-17 01:26:39 +0200

    الملفات الأولية لمشروع MakeCode

---
+++ main.ts
@@ -0,0 +1,34 @@
+function move_forward (distance: number) {
+    // التصحيح النهائي: الاسم الصحيح هو ArrowNorth
+    basic.showIcon(IconNames.Asleep)
+    // تشغيل المحركات (تأكد من توصيل الأسلاك في P0 و P1)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    // الانتظار حسب المسافة المطلوبة
+    basic.pause(distance * ms_per_cm)
+    // إيقاف المحركات
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.clearScreen()
+}
+// استقبال البيانات من الهاتف
+// تفعيل الاستماع للبيانات
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    msg = serial.readString()
+    if (msg == "F5") {
+        move_forward(5)
+    } else if (msg == "F10") {
+        move_forward(10)
+    } else if (msg == "S") {
+        // أيقونة الخطأ عند التوقف
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.showIcon(IconNames.No)
+    }
+})
+let msg = ""
+let ms_per_cm = 0
+// إعدادات المعايرة: كم ميلي ثانية لكل 1 سم؟
+ms_per_cm = 100
+// عرض كلمة GO عند بدء التشغيل
+basic.showString("GO")
