29255255be4f08a7c4264598b898ed387f673131
fikrichayma1-glitch <fikrichayma1@gmail.com>
2026-04-17 01:43:09 +0200

    حدث pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 def move_forward(distance: number):
-    # التصحيح النهائي: الاسم الصحيح هو ArrowNorth
-    basic.show_icon(IconNames.HAPPY)
-    # تشغيل المحركات (تأكد من توصيل الأسلاك في P0 و P1)
+    basic.show_icon(IconNames.ASLEEP)
     pins.digital_write_pin(DigitalPin.P0, 1)
     pins.digital_write_pin(DigitalPin.P1, 1)
-    # الانتظار حسب المسافة المطلوبة
     basic.pause(distance * ms_per_cm)
-    # إيقاف المحركات
     pins.digital_write_pin(DigitalPin.P0, 0)
     pins.digital_write_pin(DigitalPin.P1, 0)
     basic.clear_screen()
-# استقبال البيانات من الهاتف
-# تفعيل الاستماع للبيانات
 
 def on_data_received():
     global msg
@@ -21,7 +15,6 @@ def on_data_received():
     elif msg == "F10":
         move_forward(10)
     elif msg == "S":
-        # أيقونة الخطأ عند التوقف
         pins.digital_write_pin(DigitalPin.P0, 0)
         pins.digital_write_pin(DigitalPin.P1, 0)
         basic.show_icon(IconNames.NO)
@@ -29,7 +22,5 @@ serial.on_data_received(serial.delimiters(Delimiters.NEW_LINE), on_data_received
 
 msg = ""
 ms_per_cm = 0
-# إعدادات المعايرة: كم ميلي ثانية لكل 1 سم؟
 ms_per_cm = 100
-# عرض كلمة GO عند بدء التشغيل
 basic.show_string("GO")
--- main.ts
+++ main.ts
@@ -1,19 +1,13 @@
 function move_forward(distance: number) {
-    //  التصحيح النهائي: الاسم الصحيح هو ArrowNorth
-    basic.showIcon(IconNames.Happy)
-    //  تشغيل المحركات (تأكد من توصيل الأسلاك في P0 و P1)
+    basic.showIcon(IconNames.Asleep)
     pins.digitalWritePin(DigitalPin.P0, 1)
     pins.digitalWritePin(DigitalPin.P1, 1)
-    //  الانتظار حسب المسافة المطلوبة
     basic.pause(distance * ms_per_cm)
-    //  إيقاف المحركات
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 0)
     basic.clearScreen()
 }
 
-//  استقبال البيانات من الهاتف
-//  تفعيل الاستماع للبيانات
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function on_data_received() {
     
     msg = serial.readString()
@@ -22,7 +16,6 @@ serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function on_data_re
     } else if (msg == "F10") {
         move_forward(10)
     } else if (msg == "S") {
-        //  أيقونة الخطأ عند التوقف
         pins.digitalWritePin(DigitalPin.P0, 0)
         pins.digitalWritePin(DigitalPin.P1, 0)
         basic.showIcon(IconNames.No)
@@ -31,7 +24,5 @@ serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function on_data_re
 })
 let msg = ""
 let ms_per_cm = 0
-//  إعدادات المعايرة: كم ميلي ثانية لكل 1 سم؟
 ms_per_cm = 100
-//  عرض كلمة GO عند بدء التشغيل
 basic.showString("GO")
